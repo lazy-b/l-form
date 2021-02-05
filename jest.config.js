9b8d44bfb1a7888921a7a5232dc474b12ace3408
lazy-b <2280042554@qq.com>
2021-02-05 14:40:39 +0800

    feat(test): 添加单元测试

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
-}
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(lodash|lodash-es))'],
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testPathIgnorePatterns: ['<rootDir>/.history/', '<rootDir>/node_modules/'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
---
+++ src/components/form/__tests__/index.spec.js
@@ -0,0 +1,42 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import SimpleForm from '../demo/simple';
+import RulesForm from '../demo/rules';
+import UiForm from '../demo/ui';
+import LinkForm from '../demo/link';
+import TextForm from '../demo/text';
+import SlotForm from '../demo/slot';
+import DemoContainer from '../demo';
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+
+test('DemoContainer', () => {
+  const wrapper = mount(DemoContainer, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
+test('SimpleForm', () => {
+  const wrapper = mount(SimpleForm, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
+test('RulesForm', () => {
+  const wrapper = mount(RulesForm, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
+test('UiForm', () => {
+  const wrapper = mount(UiForm, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
+test('LinkForm', () => {
+  const wrapper = mount(LinkForm, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
+test('TextForm', () => {
+  const wrapper = mount(TextForm, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
+test('SlotForm', () => {
+  const wrapper = mount(SlotForm, { localVue });
+  expect(wrapper).toMatchSnapshot();
+});
